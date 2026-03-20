68af013c6079624555850b9a02ec6950ac101211
simoleo89 <simoleo89@gmail.com>
2026-03-20 17:22:50 +0100

    feat: custom prefix system protocol layer
    Prefix parsers, composers and events:
    - UserPrefixesParser, PrefixReceivedParser, ActivePrefixUpdatedParser
    - PurchasePrefixComposer, SetActivePrefixComposer, DeletePrefixComposer, RequestPrefixesComposer
    - RoomUnitChatParser reads prefixText/Color/Icon/Effect from chat packets
    - RoomSessionChatEvent carries prefix data to client
    - RoomChatHandler passes prefix fields through event chain

--- packages/communication/src/messages/incoming/IncomingHeader.ts
+++ packages/communication/src/messages/incoming/IncomingHeader.ts
@@ -474,4 +474,9 @@ export class IncomingHeader
     public static WEEKLY_GAME2_LEADERBOARD = 2196;
     public static RENTABLE_FURNI_RENT_OR_BUYOUT_OFFER = 35;
     public static HANDSHAKE_IDENTITY_ACCOUNT = 3523;
+
+    // Custom Prefixes
+    public static USER_PREFIXES = 7001;
+    public static PREFIX_RECEIVED = 7002;
+    public static ACTIVE_PREFIX_UPDATED = 7003;
 }
