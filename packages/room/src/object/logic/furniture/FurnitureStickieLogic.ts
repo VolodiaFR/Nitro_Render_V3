39d533fc7a215ca5ad4bbd4ea47056a8d448f53c
duckietm <troop@mailtjes.com>
2026-07-09 09:58:10 +0200

    🆙 Fix the post-it en pole & security updates

--- packages/room/src/object/logic/furniture/FurnitureStickieLogic.ts
+++ packages/room/src/object/logic/furniture/FurnitureStickieLogic.ts
@@ -5,7 +5,9 @@ import { FurnitureLogic } from './FurnitureLogic';
 
 export class FurnitureStickieLogic extends FurnitureLogic
 {
-    private static STICKIE_COLORS: string[] = ['9CCEFF', 'FF9CFF', '9CFF9C', 'FFFF33'];
+    private static STICKIE_COLORS: string[] = ['FF9C9C', 'FFC69C', 'FFFF33', '9CFF9C', '9CFFE8', '9CCEFF', 'C69CFF', 'FF9CFF'];
+    private static DEFAULT_STICKIE_COLOR: string = 'FFFF33';
+    private static LEGACY_STICKIE_COLORS: string[] = ['9CCEFF', 'FF9CFF', '9CFF9C', 'FFFF33'];
 
     public getEventTypes(): string[]
     {
@@ -44,7 +46,18 @@ export class FurnitureStickieLogic extends FurnitureLogic
 
         const furnitureData = this.object.model.getValue<string>(RoomObjectVariable.FURNITURE_DATA);
 
-        let colorIndex = FurnitureStickieLogic.STICKIE_COLORS.indexOf(furnitureData);
+        if(this.object.type === 'post_it')
+        {
+            let color = furnitureData;
+
+            if(FurnitureStickieLogic.STICKIE_COLORS.indexOf(color) === -1) color = FurnitureStickieLogic.DEFAULT_STICKIE_COLOR;
+
+            this.object.model.setValue(RoomObjectVariable.FURNITURE_COLOR, parseInt(color, 16));
+
+            return;
+        }
+
+        let colorIndex = FurnitureStickieLogic.LEGACY_STICKIE_COLORS.indexOf(furnitureData);
 
         if(colorIndex < 0) colorIndex = 3;
 
