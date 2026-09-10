d0d36e140e36bca46822fe422dd167fece2da4aa
duckietm <troop@mailtjes.com>
2026-09-10 09:00:05 +0200

    🆙 Remove Floor / Wall paint

--- packages/communication/src/messages/outgoing/OutgoingHeader.ts
+++ packages/communication/src/messages/outgoing/OutgoingHeader.ts
@@ -592,6 +592,7 @@ export class OutgoingHeader
     public static SET_ACTIVE_NICK_ICON = 7017;
     public static PURCHASE_CATALOG_PREFIX = 7018;
     public static SET_DISPLAY_ORDER = 7019;
+    public static ROOM_REMOVE_BACKGROUND = 7020;
 
     // YouTube Room Broadcast
     public static YOUTUBE_ROOM_PLAY = 8001;
